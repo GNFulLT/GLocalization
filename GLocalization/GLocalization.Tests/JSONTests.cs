5723ddf18c9caeb9c4240ce4b4ebb57a7607ef21
GnfFullt <73427021+GNFulLT@users.noreply.github.com>
2022-07-14 17:10:47 +0300

    Tests

---
+++ GLocalization/GLocalization.Tests/JSONTests.cs
@@ -0,0 +1,25 @@
+﻿using GLocalization.Conceretes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLocalization.Tests
+{
+    internal class JSONTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void InitTest()
+        {
+            DefaultLocalizationManager.Init("GLocalization.Tests.Resources.English.locale.json");
+            Assert.Pass();
+        }
+    }
+}
